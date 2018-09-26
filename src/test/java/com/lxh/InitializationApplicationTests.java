a850698e900da7c772b6b544378c8c0a72828c04
wangming2_tydic <wangming2@tydic.com>
2018-09-26 15:54:44 +0800

    添加单元测试代码

--- src/test/java/com/lxh/InitializationApplicationTests.java
+++ src/test/java/com/lxh/InitializationApplicationTests.java
@@ -1,16 +1,25 @@
 package com.lxh;
 
+import com.lxh.pojo.User;
+import com.lxh.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InitializationApplicationTests {
 
+	@Resource
+	private UserService userService;
+
 	@Test
-	public void contextLoads() {
+	public void test1() {
+		User user = userService.getUserById(1);
+		System.out.println(user.toString());
 	}
 
 }
