e9a3dd95a82f9950d1ba3ff7584415fc092c2ecd
wangming2_tydic <wangming2@tydic.com>
2018-09-26 11:17:59 +0800

    添加toString

--- src/main/java/com/lxh/pojo/User.java
+++ src/main/java/com/lxh/pojo/User.java
@@ -37,4 +37,13 @@ public class User {
     private String password;
 
 
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("User{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
